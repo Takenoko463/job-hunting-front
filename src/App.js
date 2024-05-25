893fdb4db7ddcb230ac3f0e2c885a35c1542e29a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-26 01:01:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 
 function App() {
+  const [greeting, setGreeting] = useState("")
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const response = await axios.get("http://43.207.6.215/api/greeting")
+        setGreeting(response.data.greeting)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{greeting}</p>
     </div>
-  );
+  )
 }
 
 export default App;
+
